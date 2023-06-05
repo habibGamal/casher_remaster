c815454c3172c22887408ca9bad7f9e290f713c8
habib gamal <habibmisi3@gmail.com>
2023-06-05 22:19:24 +0300

    invoices

--- resources/js/views/components/DisplayModel.tsx
+++ resources/js/views/components/DisplayModel.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from "react";
-import { ModelDisplayServices } from "../../services/ModelDisplayServices";
+import { ModelDisplayBase } from "./bases/ModelDisplayBase";
 import { Col, Row } from "antd";
 import PageTitle from "./PageTitle";
 import ModelConfig from "../../interfaces/ModelConfig";
@@ -15,13 +15,13 @@ export default function DisplayModel({
     className?: string;
 }) {
     // why using memo here ?
-    // this `new Model(config)` is not that expensive to rerun but!
+    // this `new ModelDisplayBase(config)` is not that expensive to rerun but!
     // it initate all states and components and when this component
     // rerun for any reason this will cause the class recreated again
     // and recreate all states so we lose where we are
     // so saving it in memo will prevent its recreation
     const model = useMemo(
-        () => new ModelDisplayServices(config, ModelForm),
+        () => new ModelDisplayBase(config, ModelForm),
         []
     );
     return (
--- resources/js/views/components/DisplayModelAsModal.tsx
+++ resources/js/views/components/DisplayModelAsModal.tsx
@@ -1,18 +1,16 @@
 import React, { useMemo } from "react";
-import { ModelDisplayServices } from "../../services/ModelDisplayServices";
+import { ModelDisplayBase } from "./bases/ModelDisplayBase";
 import { Col, Row } from "antd";
-import PageTitle from "./PageTitle";
 import ModelConfig from "../../interfaces/ModelConfig";
-import ModelForm from "../../interfaces/ModelForm";
 
 export default function DisplayModelAsModal({config}:{config:ModelConfig}) {
     // why using memo here ?
-    // this `new Model(config)` is not that expensive to rerun but!
+    // this `new ModelDisplayBase(config)` is not that expensive to rerun but!
     // it initate all states and components and when this component
     // rerun for any reason this will cause the class recreated again
     // and recreate all states so we lose where we are
     // so saving it in memo will prevent its recreation
-    const model = useMemo(() => new ModelDisplayServices(config), []);
+    const model = useMemo(() => new ModelDisplayBase(config), []);
     return (
         <Row gutter={[0, 25]} className="m-8">
             <model.Ctx>
